1769a0b6dd20dd7e847073bbc24f19270c93bec9
Paulo <paulosergiovc12@gmail.com>
2025-02-06 17:50:24 -0300

    update settings

--- app/settings.py
+++ app/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'delivery',
+    'orders',
+    'payments',
+    'promotions',
+    'restaurants',
+    'reviews',
+    'support',
 ]
 
 MIDDLEWARE = [
