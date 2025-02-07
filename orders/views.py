4cf020dc9bc3dff67f7a875b2b8c5d2284a0e800
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-07 19:12:11 -0300

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,14 @@
+from rest_framework import generics
 from .models import Order, OrderItem
-from rest_framework import viewsets
 from .serializers import OrderSerializer, OrderItemSerializer
 
 
-class OrderViewSet(viewsets.ModelViewSet):
+class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
-class OrderItemViewSet(viewsets.ModelViewSet):
-    queryset = OrderItem.objects.all()
-    serializer_class = OrderItemSerializer
+
+class OrderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
