e003d0784580a4640a6f3c8a60106e5a4063a658
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-07 19:12:39 -0300

    Update models

--- orders/models.py
+++ orders/models.py
@@ -19,7 +19,7 @@ class Order(models.Model):
     client_address = models.TextField()
     client_contact = models.CharField(max_length=20)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-    instructions = models.TextField()
+    instructions = models.TextField(blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
     created_at = models.DateTimeField(auto_now_add=True)
 
