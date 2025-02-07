a7b2ae8bb4d70e4f7ec42cb52d7d862917f5718d
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-07 19:10:00 -0300

    Update views.py

--- support/views.py
+++ support/views.py
@@ -1,8 +1,19 @@
 from .models import SupportTicket
-from rest_framework import viewsets
+from rest_framework import generics
 from .serializers import SupportTicketSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
-class SupportTicketViewSet(viewsets.ModelViewSet):
+class SupportTicketListCreateView(generics.ListCreateAPIView):
     queryset = SupportTicket.objects.all()
     serializer_class = SupportTicketSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(client=self.request.user)
+
+
+class SupportTicketRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SupportTicket.objects.all()
+    serializer_class = SupportTicketSerializer
+    permission_classes = [IsAuthenticated]
