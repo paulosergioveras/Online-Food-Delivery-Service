4588d4df882187385c1ca8cd7cb2aecf9447ccb0
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-09 17:44:28 -0300

    update admin.py

--- promotions/admin.py
+++ promotions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from promotions.models import Promotion
 
-# Register your models here.
+
+@admin.register(Promotion)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('restaurant', 'code', 'discount_percentage', 'valid_from', 'valid_until', 'is_active')
+
+
