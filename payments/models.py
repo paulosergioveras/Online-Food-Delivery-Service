873da779e5dd7d166ae51b5a8ff54861e1ab61c0
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-09 13:56:45 -0300

    improve modelds

--- payments/models.py
+++ payments/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from orders.models import Order
 
 
-class Payment(models.Model):
-    STATUS_CHOICES = [
+STATUS_CHOICES = [
         ('PENDING', 'Pending'),
         ('SUCCESS', 'Success'),
         ('FAILED', 'Failed'),
     ]
 
+
+class Payment(models.Model):
     order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='payment')
     payment_method = models.CharField(max_length=50)
     transaction_id = models.CharField(max_length=100, unique=True)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
     created_at = models.DateTimeField(auto_now_add=True)
 
