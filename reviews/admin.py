4ed2035337a74502e3c74a493642986060bacdbf
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-09 15:48:56 -0300

    update admin.py

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from reviews.models import Reviews
 
-# Register your models here.
+
+@admin.register(Reviews)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('client', 'restaurant', 'rating', 'comments', 'created_at')
