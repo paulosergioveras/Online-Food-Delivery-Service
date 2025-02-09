5f197af9a086462368a27d839f9d9518047584ba
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-09 15:18:56 -0300

    update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,36 +1,12 @@
 from django.urls import path, include
 from django.contrib import admin
 
-from restaurants.views import (
-    RestaurantListCreateView,
-    RestaurantRetrieveUpdateDestroyView,
-    MenuListCreateView,
-    MenuRetrieveUpdateDestroyView
-    )
-
-from reviews.views import ReviewsListCreateView, ReviewsRetrieveUpdateDestroyView
-from support.views import SupportTicketListCreateView, SupportTicketRetrieveUpdateDestroyView
-
-from orders.views import OrderListCreateView, OrderRetrieveUpdateDestroyView
-    
-    
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('restaurants/', RestaurantListCreateView.as_view(), name='restaurant-list-create'),
-    path('restaurants/<int:pk>/', RestaurantRetrieveUpdateDestroyView.as_view(), name='restaurant-detail'),
-
-    path('menus/', MenuListCreateView.as_view(), name='menus-list-create'),
-    path('menus/<int:pk>/', MenuRetrieveUpdateDestroyView.as_view(), name='menus-detail'),
-
-    path('reviews/', ReviewsListCreateView.as_view(), name='reviews-list-create'),
-    path('reviews/<int:pk>/', ReviewsRetrieveUpdateDestroyView.as_view(), name='reviews-detail'),
-
-    path('support/', SupportTicketListCreateView.as_view(), name='support-list-create'),
-    path('support/<int:pk>/', SupportTicketRetrieveUpdateDestroyView.as_view(), name='support-detail'),
-
-    path('orders/', OrderListCreateView.as_view(), name='order-list-create'),
-    path('orders/<int:pk>/', OrderRetrieveUpdateDestroyView.as_view(), name='order-detail'),
+    path('api/v1/', include('restaurants.urls')),
+    path('api/v1/', include('reviews.urls')),
+    path('api/v1/', include('support.urls')),
+    path('api/v1/', include('orders.urls')),
 ]
