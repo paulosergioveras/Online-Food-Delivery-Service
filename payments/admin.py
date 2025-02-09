02105d0f54d084094a96c97c12f1b5d30c938446
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-09 17:44:54 -0300

    update admin.py

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from payments.models import Payment
 
-# Register your models here.
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'payment_method', 'transaction_id', 'total_price', 'status', 'created_at')
+    search_fields = ('transaction_id', 'order__id', 'payment_method')
+    readonly_fields = ('created_at',)
+
+    
+    def order(self, obj):
+        return f'Order #{obj.order.id}'
+    order.short_description = 'Order'  # Nome da coluna na lista
