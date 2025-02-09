b6b489882c6be5c83fd13d399ef07a3f0d7a28e5
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-09 15:51:05 -0300

    remove menu urls

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -5,7 +5,4 @@ from . import views
 urlpatterns = [
     path('restaurants/', views.RestaurantListCreateView.as_view(), name='restaurant-list-create'),
     path('restaurants/<int:pk>/', views.RestaurantRetrieveUpdateDestroyView.as_view(), name='restaurant-detail'),
-
-    path('menus/', views.MenuListCreateView.as_view(), name='menus-list-create'),
-    path('menus/<int:pk>/', views.MenuRetrieveUpdateDestroyView.as_view(), name='menus-detail'),
 ]
