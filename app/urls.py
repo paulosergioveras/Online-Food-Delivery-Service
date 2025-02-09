6786e2b3fd5f1a5ef9d7bb2167a3aa6255ed07c8
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-09 15:51:19 -0300

    add menu urls

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/v1/', include('restaurants.urls')),
+    path('api/vi/', include('menu.urls')),
     path('api/v1/', include('reviews.urls')),
     path('api/v1/', include('support.urls')),
     path('api/v1/', include('orders.urls')),
