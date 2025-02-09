fdac2414268d82a3d918e07a16600c89dea4667e
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-09 17:04:00 -0300

    add some adjusts

--- support/admin.py
+++ support/admin.py
@@ -4,4 +4,4 @@ from support.models import SupportTicket
 
 @admin.register(SupportTicket)
 class ModelAdmin(admin.ModelAdmin):
-	list_display = ('client', 'order', 'subject', 'description', 'status',' created_at')
+	list_display = ('client', 'order', 'subject', 'description', 'status', 'created_at')
