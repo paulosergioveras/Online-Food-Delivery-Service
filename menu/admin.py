ea41765edbd318c4bb23ac0804ea1f6a32b5145a
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-10 00:13:18 -0300

    Update admin.py

--- menu/admin.py
+++ menu/admin.py
@@ -4,4 +4,4 @@ from menu.models import Menu
 
 @admin.register(Menu)
 class ModelAdmin(admin.ModelAdmin):
-	list_display = ('dish_name', 'dish_description', 'restaurant', 'price', 'is_active')
+    list_display = ('dish_name', 'dish_description', 'restaurant', 'price', 'is_active')
