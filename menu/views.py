1058dd8088502de3c30c4baab9741a176d31cf06
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-10 00:13:14 -0300

    Update views.py

--- menu/views.py
+++ menu/views.py
@@ -2,22 +2,30 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermission
 from menu.models import Menu
-from menu.serializers import MenuSerializer
+from menu.serializers import MenuSerializer, MenuListDetailSerializer
 from restaurants.models import Restaurant
 
 
 class MenuListCreateView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
+    
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return MenuListDetailSerializer
+        return MenuSerializer
+    
 
     def perform_create(self, serializer):
         restaurant = Restaurant.objects.get(owner=self.request.user)
         serializer.save(restaurant=restaurant)
 
-    
+
 class MenuRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
-    
+
+
+
