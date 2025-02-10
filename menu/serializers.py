2a93b85c91ad3fdc3795b7a4dadaaa014b062fb0
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-10 00:13:16 -0300

    Update serializers.py

--- menu/serializers.py
+++ menu/serializers.py
@@ -4,14 +4,22 @@ from restaurants.serializers import RestaurantSerializer
 from restaurants.models import Restaurant
 
 
-
 class MenuSerializer(serializers.ModelSerializer):
     restaurant = RestaurantSerializer(read_only=True)
     restaurant_id = serializers.PrimaryKeyRelatedField(
         queryset=Restaurant.objects.all(), source='restaurant', write_only=True
     )
 
-  
+
     class Meta:
         model = Menu
         fields = '__all__'
+
+
+
+class MenuListDetailSerializer(serializers.ModelSerializer):
+    restaurant = RestaurantSerializer()
+
+    class Meta:
+        model = Menu
+        fields = ['id', 'dish_name', 'dish_description', 'restaurant', 'price', 'is_active']
