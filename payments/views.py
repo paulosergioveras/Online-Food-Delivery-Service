8a4c40e57cc0742926f37a7fa717d8c709f743ef
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-09 21:13:34 -0300

    update views.py

--- payments/views.py
+++ payments/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.conf import settings
 import stripe
+from rest_framework.permissions import IsAuthenticated
 from .models import Payment
 from .serializers import PaymentSerializer
 
@@ -11,16 +12,19 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class PaymentListCreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
 
 
 class PaymentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
 
 
 class CreatePaymentIntentAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
     def post(self, request):
         order_id = request.data.get('order_id')
         total_price = request.data.get('total_price')
--- promotions/views.py
+++ promotions/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Promotion
 from .serializers import PromotionSerializer
 
 
 class PromotionListCreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Promotion.objects.all()
     serializer_class = PromotionSerializer
 
 
 class PromotionRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Promotion.objects.all()
     serializer_class = PromotionSerializer
