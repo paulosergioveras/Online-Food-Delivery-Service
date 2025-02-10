e8bdfd04ab7167648cb97ac31105bbec2e5b3d3e
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-10 00:12:42 -0300

    Update views.py

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,8 +1,10 @@
-from rest_framework import generics
+from django.db.models import Count, Avg
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermission
 from restaurants.serializers import RestaurantSerializer
 from restaurants.models import Restaurant
+from reviews.models import Reviews
 
 
 class RestaurantListCreateView(generics.ListCreateAPIView):
@@ -18,6 +20,25 @@ class RestaurantRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView)
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
+
+
+class RestaurantStatusView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Restaurant.objects.all()
+
+    def get(self, request):
+        total_restaurants = self.queryset.count()
+        total_reviews = Reviews.objects.count()
+        average_rating = Reviews.objects.aggregate(avg_rating=Avg('rating'))['avg_rating']
+
     
+        return response.Response(
+            data = {
+                'total_movies': total_restaurants,
+                'total_reviews': total_reviews,
+                'average_rating': round(average_rating, 1) if average_rating else 0,
+            },
+            status=status.HTTP_200_OK,
+        )
 
 
