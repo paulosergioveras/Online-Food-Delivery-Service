b8d87e2a976ade427fc5ab8e936e1f9414892cb2
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-10 00:13:30 -0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
 
     path('api/v1/', include('authentication.urls')),
     path('api/v1/', include('restaurants.urls')),
-    path('api/vi/', include('menu.urls')),
     path('api/v1/', include('reviews.urls')),
     path('api/v1/', include('support.urls')),
     path('api/v1/', include('orders.urls')),
     path('api/v1/', include('promotions.urls')),
-    path('api/v1/', include('payments.urls'))
+    path('api/v1/', include('payments.urls')),
+    path('api/v1/', include('menu.urls')),
 ]
